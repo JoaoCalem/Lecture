bb033448d00e3eccac23e46890566db6001dd8f6
joao calem <joaocalem@gmail.com>
2023-06-13 10:53:35 +0100

    webcam test¨

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
-import streamlit as st
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
+import av
+import cv2
 
-import numpy as np
-import pandas as pd
-import tensorflow as tf
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+    image = frame.to_ndarray(format="bgr24")
+    cv2.putText(
+            image,
+            str(image.mean()),
+            (50,50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (255, 0, 0),
+            2,
+        )
+    return av.VideoFrame.from_ndarray(image, format="bgr24")
 
-st.markdown("""# Displaying a dataframe!!!!!!!!!
-## This is a sub header
-This is text""")
 
-df = pd.DataFrame({
-    'first column': list(range(1, 11)),
-    'second column': np.arange(10, 101, 10)
-})
-
-
-# this slider allows the user to select a number of lines
-# to display in the dataframe
-# the selected value is returned by st.slider
-line_count = st.slider('Select a line count', 1, 10, 3)
-
-# and used to select the displayed lines
-head_df = df.head(line_count)
-
-head_df
-
-hello = tf.constant("hello TensorFlow!")
-
-hello
+webrtc_ctx = webrtc_streamer(
+    key="object-detection",
+    mode=WebRtcMode.SENDRECV,
+    video_frame_callback=video_frame_callback,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+)
