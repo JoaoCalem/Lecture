2e5b9dec0f529e6ce80eb79323f32f5062f6571a
joao calem <joaocalem@gmail.com>
2023-06-13 11:40:41 +0100

    webcam test¨

--- app.py
+++ app.py
@@ -1,35 +1,16 @@
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
-import streamlit as st
-import os
-import logging
 import av
 import cv2
+from twilio.rest import Client
+import streamlit as st
 
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = st.secrets['sid']
+auth_token = st.secrets['token']
+client = Client(account_sid, auth_token)
 
-@st.cache_data
-def get_ice_servers():
-    """Use Twilio's TURN server because Streamlit Community Cloud has changed
-    its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
-    We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
-    but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656  # noqa: E501
-    See https://github.com/whitphx/streamlit-webrtc/issues/1213
-    """
-
-    # Ref: https://www.twilio.com/docs/stun-turn/api
-    try:
-        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-    except KeyError:
-        logger.warning(
-            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
-        )
-        return [{"urls": ["stun:stun.l.google.com:19302"]}]
-
-    client = Client(account_sid, auth_token)
-
-    token = client.tokens.create()
-
-    return token.ice_servers
+token = client.tokens.create()
 
 def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     image = frame.to_ndarray(format="bgr24")
@@ -44,14 +25,13 @@ def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
         )
     return av.VideoFrame.from_ndarray(image, format="bgr24")
 
-logger = logging.getLogger(__name__)
 
 webrtc_ctx = webrtc_streamer(
     key="object-detection",
     mode=WebRtcMode.SENDRECV,
     video_frame_callback=video_frame_callback,
     rtc_configuration={
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+      "iceServers": token.ice_servers
     },
     media_stream_constraints={"video": True, "audio": False},
     async_processing=True,
--- pages/page2.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-
-st.secrets['my_secret']
-
-st.secrets.sec1['my_secret2']
